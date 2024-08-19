6b4fff84d9116b008a86931be6e521c55fed2715
Kylian Schmidt <kylian.schmidt@student.kit.edu>
2024-08-19 17:08:53 +0200

    AIDO class can be called with a list of SimulationParameters instead of passing a SimulationParameterDictionary

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import os
 from modules.aido import AIDO  # required
-from modules.simulation_helpers import SimulationParameter, SimulationParameterDictionary  # required
+from modules.simulation_helpers import SimulationParameter  # required
 from container_examples.calo_opt.calo_opt_interface import AIDOUserInterfaceExample  # Import your derived class
 
 global_htcondor_settings = {
@@ -22,16 +22,14 @@ global_htcondor_settings = {
 if __name__ == "__main__":
     os.system("rm ./results -rf")  # remove everything from results
 
-    sim_param_dict = SimulationParameterDictionary([
-        SimulationParameter('thickness_absorber_0', 1.0, min_value=1E-3, max_value=5.0, sigma=0.2),
-        SimulationParameter('thickness_absorber_1', 1.0, min_value=1E-3, max_value=5.0, sigma=0.2),
-        SimulationParameter('thickness_scintillator_0', 0.5, min_value=1E-3, max_value=1.0, sigma=0.2),
-        SimulationParameter('thickness_scintillator_1', 0.1, min_value=1E-3, max_value=1.0, sigma=0.2),
-        SimulationParameter("num_events", 100, optimizable=False)
-    ])
-
     AIDO(
-        sim_param_dict,
+        parameters=[
+            SimulationParameter('thickness_absorber_0', 1.0, min_value=1E-3, max_value=5.0, sigma=0.2),
+            SimulationParameter('thickness_absorber_1', 1.0, min_value=1E-3, max_value=5.0, sigma=0.2),
+            SimulationParameter('thickness_scintillator_0', 0.5, min_value=1E-3, max_value=1.0, sigma=0.2),
+            SimulationParameter('thickness_scintillator_1', 0.1, min_value=1E-3, max_value=1.0, sigma=0.2),
+            SimulationParameter("num_events", 100, optimizable=False)
+        ],
         user_interface=AIDOUserInterfaceExample,
         simulation_tasks=4,
         threads=5
