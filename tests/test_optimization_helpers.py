0a7e5fc3529e36c68979db6cd9503d0ad488b53e
Kylian Schmidt <kylian.schmidt@student.kit.edu>
2024-10-16 09:20:03 +0200

    Removed reference to inexistant file in test_optimization_helpers.py

--- tests/test_optimization_helpers.py
+++ tests/test_optimization_helpers.py
@@ -6,9 +6,37 @@ from modules.simulation_helpers import SimulationParameterDictionary
 
 @pytest.fixture
 def parameter_dict():
-    return SimulationParameterDictionary.from_json(
-        "results_old/results_discrete_20241014/parameters/param_dict_iter_0.json"
-    )
+    return SimulationParameterDictionary([
+        AIDO.parameter(
+            "thickness_absorber",
+            5.6,
+            min_value=0.01,
+            max_value=20.0,
+            sigma=0.2,
+            cost=1.1,
+        ),
+        AIDO.parameter(
+            "thickness_scintillator",
+            0.1,
+            min_value=0.05,
+            max_value=1.0,
+            sigma=0.2,
+            cost=5.0,
+        ),
+        AIDO.parameter(
+            "absorber_material",
+            "G4_Pb",
+            discrete_values=["G4_Pb", "G4_W", "G4_Fe"],
+            cost=[1.3, 0.26, 0.092],
+        ),
+        AIDO.parameter(
+            "num_blocks",
+            3,
+            discrete_values=list(range(1, 10)),
+            cost=(0.1 * np.arange(1, 10)).tolist(),
+        ),
+        AIDO.parameter("num_events", 200, optimizable=False),
+    ])
 
 
 def test_instantiation(parameter_dict):
