c632286418393ed5d5d5df89779dff3d040ce441
Kylian Schmidt <kylian.schmidt@student.kit.edu>
2024-10-17 16:39:03 +0200

    Formatting and added test

--- container_examples/calo_opt/interface_calo_opt.py
+++ container_examples/calo_opt/interface_calo_opt.py
@@ -129,7 +129,7 @@ class AIDOUserInterfaceExample(AIDOUserInterface):
             parameter_dict["thickness_absorber"].current_value
             + parameter_dict["thickness_scintillator"].current_value
         )
-        detector_length_loss = torch.mean(0.5 * torch.nn.ReLU()(torch.tensor(detector_length - 100.0)))
+        detector_length_loss = torch.mean(torch.nn.ReLU()(torch.tensor(detector_length - 100.0)))
         return material_costs + detector_length_loss
 
 
@@ -142,8 +142,12 @@ if __name__ == "__main__":
         AIDO.parameter("num_blocks", 10, optimizable=False),
         AIDO.parameter("num_events", 400, optimizable=False)
     ])
+    sim_param_expensive = SimulationParameterDictionary([
+        AIDO.parameter("thickness_absorber", 1.0, units="cm", max_value=50.0, min_value=0.1, sigma=0.5),
+        AIDO.parameter("thickness_scintillator", 0.5, units="cm", max_value=10.0, min_value=0.01, sigma=0.5),
+        AIDO.parameter("absorber_material", "G4_Pb", discrete_values=["G4_Pb", "G4_Fe"], cost=[1.3, 0.092]),
+        AIDO.parameter("scintillator_material", "G4_PbWO4", discrete_values=["G4_PbWO4", "G4_Fe"], cost=[1.5, 1.0]),
+        AIDO.parameter("num_blocks", 10, optimizable=False),
+        AIDO.parameter("num_events", 400, optimizable=False)
+    ])
     print(f"{AIDOUserInterfaceExample().constraints(sim_param_cheap)=}")
-    sim_param_expensive = SimulationParameterDictionary.from_json(
-        "results_comparison/results_20241017_2/parameters/param_dict_iter_38.json"
-    )
-    print(f"{AIDOUserInterfaceExample().constraints(sim_param_expensive)=}")
