d8dc0adf2e593b5cfd0095138edad4190c03713b
Kylian Schmidt <kylian.schmidt@student.kit.edu>
2024-10-21 14:08:15 +0200

    Changed scaling of parameter boundaries from SoftMax to ReLU to avoid base loss contribution when it should be off

--- modules/optimizer.py
+++ modules/optimizer.py
@@ -73,8 +73,8 @@ class Optimizer(torch.nn.Module):
         if len(self.parameter_box) != 0:
             parameters_continuous_tensor = self.parameter_module.tensor("continuous")
             return (
-                torch.mean(10. * torch.nn.Softmax()(self.parameter_box[:, 0] - parameters_continuous_tensor))
-                + torch.mean(10. * torch.nn.Softmax()(- self.parameter_box[:, 1] + parameters_continuous_tensor))
+                torch.mean(100. * torch.nn.ReLU()(self.parameter_box[:, 0] - parameters_continuous_tensor))
+                + torch.mean(100. * torch.nn.ReLU()(- self.parameter_box[:, 1] + parameters_continuous_tensor))
             )
 
     def other_constraints(
