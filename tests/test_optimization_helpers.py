2705a3a823a4f4dbd2e8281b2eb767ce2659be66
Kylian Schmidt <kylian.schmidt@student.kit.edu>
2024-10-29 11:41:42 +0100

    Updated tests to fit new package naming convention

--- tests/test_optimization_helpers.py
+++ tests/test_optimization_helpers.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pytest
 
-import aido.main as main
 from aido.optimization_helpers import ParameterModule
+from aido.simulation_helpers import SimulationParameter, SimulationParameterDictionary
 
 
 @pytest.fixture
 def parameter_dict():
-    return main.SimulationParameterDictionary([
-        main.SimulationParameter(
+    return SimulationParameterDictionary([
+        SimulationParameter(
             "thickness_absorber",
             5.6,
             min_value=0.01,
@@ -16,7 +16,7 @@ def parameter_dict():
             sigma=0.2,
             cost=1.1,
         ),
-        main.SimulationParameter(
+        SimulationParameter(
             "thickness_scintillator",
             0.1,
             min_value=0.05,
@@ -24,19 +24,19 @@ def parameter_dict():
             sigma=0.2,
             cost=5.0,
         ),
-        main.SimulationParameter(
+        SimulationParameter(
             "absorber_material",
             "G4_Pb",
             discrete_values=["G4_Pb", "G4_W", "G4_Fe"],
             cost=[1.3, 0.26, 0.092],
         ),
-        main.SimulationParameter(
+        SimulationParameter(
             "num_blocks",
             3,
             discrete_values=list(range(1, 10)),
             cost=(0.1 * np.arange(1, 10)).tolist(),
         ),
-        main.SimulationParameter("num_events", 200, optimizable=False),
+        SimulationParameter("num_events", 200, optimizable=False),
     ])
 
 
--- tests/test_simulation_helpers.py
+++ tests/test_simulation_helpers.py
@@ -1,21 +1,21 @@
 import pytest
 
-import aido.main as main
+from aido.simulation_helpers import SimulationParameter, SimulationParameterDictionary
 
 
 @pytest.fixture
-def sim_param_dict() -> main.SimulationParameterDictionary:
-    return main.SimulationParameterDictionary([
-        main.SimulationParameter("absorber_thickness", 10.0, min_value=0.5, max_value=40.0),
-        main.SimulationParameter("absorber_material", "LEAD", discrete_values=["LEAD", "TUNGSTEN"]),
-        main.SimulationParameter("energy", 1000, optimizable=False),
-        main.SimulationParameter("num_absorber_plates", 2, discrete_values=list(range(0, 5))),
+def sim_param_dict() -> SimulationParameterDictionary:
+    return SimulationParameterDictionary([
+        SimulationParameter("absorber_thickness", 10.0, min_value=0.5, max_value=40.0),
+        SimulationParameter("absorber_material", "LEAD", discrete_values=["LEAD", "TUNGSTEN"]),
+        SimulationParameter("energy", 1000, optimizable=False),
+        SimulationParameter("num_absorber_plates", 2, discrete_values=list(range(0, 5))),
     ])
 
 
 def test_save_load(sim_param_dict):
     sim_param_dict.to_json("./sim_param_dict")
-    main.SimulationParameterDictionary.from_json("./sim_param_dict")
+    SimulationParameterDictionary.from_json("./sim_param_dict")
 
 
 def test_generate_new(sim_param_dict):
@@ -38,17 +38,17 @@ def test_to_df(sim_param_dict):
 
 def test_cost():
     with pytest.raises(AssertionError):
-        main.SimulationParameter("foo", 1.0, cost=-10)
-        main.SimulationParameter("foo", 1.0, cost=[0.1, 2.0])
-        main.SimulationParameter("foo", 1, discrete_values=[1, 2, 3], cost=1.7)
+        SimulationParameter("foo", 1.0, cost=-10)
+        SimulationParameter("foo", 1.0, cost=[0.1, 2.0])
+        SimulationParameter("foo", 1, discrete_values=[1, 2, 3], cost=1.7)
 
 
 def test_weighted_cost():
     assert (
-        main.SimulationParameter("foo", 2.3, cost=7.2).weighted_cost == 2.3 * 7.2
+        SimulationParameter("foo", 2.3, cost=7.2).weighted_cost == 2.3 * 7.2
     )
     assert (
-        main.SimulationParameter(
+        SimulationParameter(
             "foo", "a", discrete_values=["a", "b"], cost=[1.8, 0.5]
         ).weighted_cost == 0.5 * 1.8 + 0.5 * 0.5
     )
