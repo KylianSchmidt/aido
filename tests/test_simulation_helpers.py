f805bd1103c649ac4b431659e611b00014a3dda8
Kylian Schmidt <kylian.schmidt@student.kit.edu>
2024-10-31 16:02:33 +0100

    Updated imports in tests

--- tests/test_simulation_helpers.py
+++ tests/test_simulation_helpers.py
@@ -1,21 +1,21 @@
 import pytest
 
-from aido import SimulationParameter, SimulationParameterDictionary
+import aido
 
 
 @pytest.fixture
-def sim_param_dict() -> SimulationParameterDictionary:
-    return SimulationParameterDictionary([
-        SimulationParameter("absorber_thickness", 10.0, min_value=0.5, max_value=40.0),
-        SimulationParameter("absorber_material", "LEAD", discrete_values=["LEAD", "TUNGSTEN"]),
-        SimulationParameter("energy", 1000, optimizable=False),
-        SimulationParameter("num_absorber_plates", 2, discrete_values=list(range(0, 5))),
+def sim_param_dict() -> aido.SimulationParameterDictionary:
+    return aido.SimulationParameterDictionary([
+        aido.SimulationParameter("absorber_thickness", 10.0, min_value=0.5, max_value=40.0),
+        aido.SimulationParameter("absorber_material", "LEAD", discrete_values=["LEAD", "TUNGSTEN"]),
+        aido.SimulationParameter("energy", 1000, optimizable=False),
+        aido.SimulationParameter("num_absorber_plates", 2, discrete_values=list(range(0, 5))),
     ])
 
 
 def test_save_load(sim_param_dict):
     sim_param_dict.to_json("./sim_param_dict")
-    SimulationParameterDictionary.from_json("./sim_param_dict")
+    aido.SimulationParameterDictionary.from_json("./sim_param_dict")
 
 
 def test_generate_new(sim_param_dict):
@@ -38,17 +38,17 @@ def test_to_df(sim_param_dict):
 
 def test_cost():
     with pytest.raises(AssertionError):
-        SimulationParameter("foo", 1.0, cost=-10)
-        SimulationParameter("foo", 1.0, cost=[0.1, 2.0])
-        SimulationParameter("foo", 1, discrete_values=[1, 2, 3], cost=1.7)
+        aido.SimulationParameter("foo", 1.0, cost=-10)
+        aido.SimulationParameter("foo", 1.0, cost=[0.1, 2.0])
+        aido.SimulationParameter("foo", 1, discrete_values=[1, 2, 3], cost=1.7)
 
 
 def test_weighted_cost():
     assert (
-        SimulationParameter("foo", 2.3, cost=7.2).weighted_cost == 2.3 * 7.2
+        aido.SimulationParameter("foo", 2.3, cost=7.2).weighted_cost == 2.3 * 7.2
     )
     assert (
-        SimulationParameter(
+        aido.SimulationParameter(
             "foo", "a", discrete_values=["a", "b"], cost=[1.8, 0.5]
         ).weighted_cost == 0.5 * 1.8 + 0.5 * 0.5
     )
