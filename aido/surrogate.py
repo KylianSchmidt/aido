Fix normalization in surrogate model when loading datasets with normalized parameters

aido/surrogate.py
@@ -78,7 +78,8 @@ class SurrogateDataset(Dataset):
             reconstructed_key: str = "Reconstructed",
             device: str = "cuda" if torch.cuda.is_available() else "cpu",
             means: List[np.float32] | None = None,
-            stds: List[np.float32] | None = None
+            stds: List[np.float32] | None = None,
+            normalise_parameters: bool = False,
             ):
         """
         Initializes the Surrogate model with the provided DataFrame and keys. All inputs must be
@@ -102,6 +103,7 @@ class SurrogateDataset(Dataset):
         self.context = self.df[context_key].to_numpy(np.float32)
         self.targets = self.df[target_key].to_numpy(np.float32)
         self.reconstructed = self.df[reconstructed_key].to_numpy(np.float32)
+        self.normalise_parameters = normalise_parameters
 
         self.shape: List[int] = (
             self.parameters.shape[1],
@@ -130,6 +132,9 @@ class SurrogateDataset(Dataset):
         self.c_means = [torch.tensor(a).to(device) for a in self.means]
         self.c_stds = [torch.tensor(a).to(device) for a in self.stds]
 
+        if self.normalise_parameters:
+            logger.info("Normalized parameters")
+            self.parameters = self.normalise_features(self.parameters, index=0)
         self.context = self.normalise_features(self.context, index=1)
         self.targets = self.normalise_features(self.targets, index=2)
         self.reconstructed = self.normalise_features(self.reconstructed, index=2)
@@ -142,7 +147,11 @@ class SurrogateDataset(Dataset):
         df = df.dropna(axis=0, ignore_index=True)
         return df
 
-    def unnormalise_features(self, target: torch.Tensor | np.ndarray, index: int) -> torch.Tensor | np.ndarray:
+    def unnormalise_features(
+            self,
+            target: torch.Tensor | np.ndarray,
+            index: int
+            ) -> torch.Tensor | np.ndarray:
         ''' Return the physically meaningful target from the normalised target
         Index:
             0 -> Parameters
@@ -161,7 +170,6 @@ class SurrogateDataset(Dataset):
             1 -> Context
             2 -> Targets
         '''
-        assert index != 0, "Do not normalize parameters"
         if isinstance(target, torch.Tensor):
             return (target - self.c_means[index]) / self.c_stds[index]
         elif isinstance(target, np.ndarray):

examples/example_full_calorimeter.py
@@ -51,27 +51,6 @@ class UIFullCalorimeter(AIDOUserInterfaceExample):
 
     def plot(self, parameter_dict: aido.SimulationParameterDictionary | None = None) -> None:
 
-        def add_plot_header(ax: plt.Axes) -> plt.Axes:
-            plt.text(
-                0.0, 1.06,
-                "AIDO",
-                transform=ax.transAxes, fontsize=14, fontweight='bold', va='top', ha='left'
-            )
-            plt.text(
-                0.125, 1.06,
-                "Detector Optimization",
-                transform=ax.transAxes, fontsize=14, style='italic', va='top', ha='left'
-            )
-            plt.text(
-                0.01, 0.98,
-                "Sampling Calorimeter\n"
-                "50% photons and 50% pions\n"
-                r"$20 \times 400$" + " MC Events / Iteration\n"
-                r"$E_\text{true}=[1, 20]$" + " GeV",
-                transform=ax.transAxes, va='top', ha='left'
-            )
-            return ax
-
         def plot_reco_loss(
                 iteration: int,
                 file_name: str | os.PathLike,
@@ -111,7 +90,7 @@ class UIFullCalorimeter(AIDOUserInterfaceExample):
 
             handles, labels = ax.get_legend_handles_labels()
             labels, handles = zip(*sorted(zip(labels, handles)))
-            ax = add_plot_header(ax)
+            ax = self.add_plot_header(ax)
             ax.legend(handles, labels)
             plt.yscale("log")
             plt.xlim(bins[0], bins[-1])
@@ -162,7 +141,7 @@ class UIFullCalorimeter(AIDOUserInterfaceExample):
 
             handles, labels = ax.get_legend_handles_labels()
             labels, handles = zip(*sorted(zip(labels, handles)))
-            ax = add_plot_header(ax)
+            ax = self.add_plot_header(ax)
             ax.legend(handles, labels)
             plt.ylabel(f"Counts / ({(bins[1] - bins[0]):.2f} GeV" + r"$^{1/2}$" + ")")
             plt.xlabel(r"$(E_\text{rec} - E_\text{true}) / E_\text{true}^{1/2}\, \left[ \text{GeV}^{1/2} \right]$")
@@ -174,7 +153,7 @@ class UIFullCalorimeter(AIDOUserInterfaceExample):
 
         def plot_energy_resolution_first_and_last() -> None:
             fig, ax = plt.subplots()
-            ax = add_plot_header(ax)
+            ax = self.add_plot_header(ax)
             dirs = glob.glob(f"{self.results_dir}/task_outputs/iteration=*/validation=False/reco_output_df")
             cmap = plt.get_cmap('coolwarm', len(dirs))
             bins = np.linspace(-20, 20, 80 + 1)
@@ -220,7 +199,7 @@ class UIFullCalorimeter(AIDOUserInterfaceExample):
             df_materials.columns = df.columns
 
             fig, ax = plt.subplots(figsize=(8.5, 5.5))
-            ax = add_plot_header(ax)
+            ax = self.add_plot_header(ax)
             absorber_cmap = mcolors.LinearSegmentedColormap.from_list("blue_grey", ["blue", "grey"])
             scintillator_cmap = plt.get_cmap("spring")
 
@@ -255,7 +234,7 @@ class UIFullCalorimeter(AIDOUserInterfaceExample):
             plt.xlabel("Iteration")
             plt.xlim(0, len(df))
             plt.ylim(0, 220)
-            ax = add_plot_header(ax)
+            ax = self.add_plot_header(ax)
             cbar_absorber = plt.cm.ScalarMappable(cmap=absorber_cmap)
             cbar_absorber.set_array([])
             cbar1 = plt.colorbar(
@@ -307,7 +286,7 @@ class UIFullCalorimeter(AIDOUserInterfaceExample):
             df_loss: pd.DataFrame = aido.Plotting.optimizer_loss(results_dir=self.results_dir)
 
             fig, ax = plt.subplots()
-            ax = add_plot_header(ax)
+            ax = self.add_plot_header(ax)
             plt.plot(e_loss_best_array, label="Mean Reconstruction Loss")
             plt.plot(
                 np.linspace(0, df_loss["Iteration"].to_numpy()[-1], len(df_loss)),