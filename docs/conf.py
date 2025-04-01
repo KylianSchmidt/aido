516d9455a98e19f575bd3714f1da56ef049e4b85
Kylian Schmidt <kylian.schmidt@kit.edu>
2025-04-01 15:16:18 +0200

    Update Wiki towards gitlab compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,14 @@ release = "0.0.3"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+master_doc = "home"
 extensions = [
     'sphinx.ext.autodoc',           # For automatical documentation
     'sphinx_autodoc_typehints',     # Autodoc typehints
     'sphinx.ext.napoleon',          # For Google and NumPy-style docstrings
     'sphinx.ext.mathjax',           # For LaTeX math rendering,
     'myst_parser',                  # For .md files
+    "sphinx.ext.intersphinx",       # For genindex and other files
 ]
 
 templates_path = ['_templates']
--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,3 +1,8 @@
 export LC_ALL=en_US.UTF-8
 make html
 sphinx-build -b markdown . _build/markdown
+
+python -c "import html2text, sys; print(html2text.html2text(sys.stdin.read()))" < _build/html/genindex.html > _build/markdown/genindex.md
+python -c "import html2text, sys; print(html2text.html2text(sys.stdin.read()))" < _build/html/py-modindex.html > _build/markdown/py-modindex.md
+python -c "import html2text, sys; print(html2text.html2text(sys.stdin.read()))" < _build/html/search.html > _build/markdown/search.md
+
