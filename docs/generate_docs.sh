a897286d88ba8eddd1c8c5d45580c85b197e3737
Kylian Schmidt <kylian.schmidt@kit.edu>
2025-09-17 15:21:41 +0200

    Improve docs generation script

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 set -e  # Exit on error
-export LC_ALL=en_US.UTF-8
 
+# Setup directories
 WIKI_DIR="../aido.wiki"
-DOCS_DIR="../docs"
-HTML_BUILD="$DOCS_DIR/_build/html"
-
 mkdir -p "$WIKI_DIR"/{api,guides}
-rm -rf "$DOCS_DIR/_build"
 
-sphinx-build -b html . "$HTML_BUILD"
+make html
 
-for page in genindex py-modindex search; do
-    python -c "import html2text, sys; print(html2text.html2text(sys.stdin.read()))" \
-        < "$HTML_BUILD/$page.html" > "$WIKI_DIR/$page.md"
-done
+cp home.md "$WIKI_DIR/"
+cp guides/*.md "$WIKI_DIR/guides/" 2>/dev/null || true
 
-for dir in guides api; do
-    if [ -d "$HTML_BUILD/$dir" ]; then
-        cp -r "$HTML_BUILD/$dir/"* "$WIKI_DIR/$dir/"
-    fi
-done
+if [ -d "_build/html/source" ]; then
+    for module in _build/html/source/aido.*.html; do
+        if [ -f "$module" ]; then
+            basename=$(basename "$module" .html)
+            python -c "import html2text, sys; print(html2text.html2text(sys.stdin.read()))" \
+                < "$module" > "$WIKI_DIR/api/$basename.md"
+        fi
+    done
+fi \
+                < "$guide" > "$WIKI_DIR/guides/$basename.md"
+        fi
+    done
+fi
