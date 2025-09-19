6efdc04b4e47f4bdffaa4d4a80743707b72767eb
Kylian Schmidt <kylian.schmidt@kit.edu>
2025-09-19 15:42:15 +0200

    Remove mentions to __version__

--- aido/__init__.py
+++ aido/__init__.py
@@ -16,5 +16,3 @@ __all__ = [
     "Surrogate",
     "SurrogateDataset"
 ]
-
-__version__ = "0.1.3"
--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-from aido import __version__  # noqa
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -16,12 +14,11 @@ from aido import __version__  # noqa
 project = 'aido'
 copyright = '2024, Kylian Schmidt, Dr. Jan Kieseler'
 author = 'Kylian Schmidt, Dr. Jan Kieseler'
-release = __version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-master_doc = "home"
+master_doc = "index"
 extensions = [
     'autoapi.extension',            # For automatical documentation
     'sphinx_autodoc_typehints',     # Autodoc typehints
@@ -38,7 +35,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "furo"
 html_static_path = ['_static']
 
 # Add markdown files with myst_parser
@@ -56,8 +53,6 @@ autoapi_options = [
     "members",
     "show-inheritance",
     "show-module-summary",
-    "special-members",
-    "undoc-members",
     "imported-members",
 ]
 autoapi_python_class_content = "both"
@@ -80,5 +75,29 @@ suppress_warnings = [
     'toc.no_title',  # Suppress title warnings
     'docutils.definition_list_ends_without_blank_line',  # Suppress definition list warnings
     'docutils.bullet_list_ends_without_blank_line',  # Suppress bullet list warnings
-    'docutils.block_quote_ends_without_blank_line'  # Suppress block quote warnings
+    'docutils.block_quote_ends_without_blank_line',  # Suppress block quote warnings
+    'autoapi.python_import_resolution'  # Suppress autoapi duplicate warnings
 ]
+
+# Napoleon settings for better docstring parsing
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+# AutoAPI settings to handle duplicates
+autoapi_keep_files = True
+autoapi_add_objects_to_toctree = False
+autoapi_add_toctree_entry = False
+autoapi_python_use_implicit_namespaces = True
+autoapi_python_override_docstring = True
