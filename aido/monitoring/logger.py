4317a4a25c87c2f38e892792d5f3aae0ed22e00b
Tobias Kortus <tobias.kortus@outlook.de>
2025-11-12 10:22:14 +0100

    Add new options to WandbLogger

--- aido/monitoring/logger.py
+++ aido/monitoring/logger.py
@@ -11,7 +11,20 @@ from typing import Any, Optional, Sequence, Tuple
 from aido.config import AIDOConfig
 import numpy as np
 from aido.optimization_helpers import ContinuousParameter, ParameterModule
+from matplotlib import pyplot as plt
 
+
+def flatten_gradients(parameters: ParameterModule) -> torch.Tensor:
+    grads = []
+    for p in parameters.parameters():
+        if p.grad is not None:
+            grads.append(p.grad.detach().flatten())
+    
+    if len(grads) == 0:
+        raise Exception("WandbLogger: No gradients found to log.")
+
+    flat_grads = torch.cat(grads)
+    return flat_grads
 
 class WandbLogger:
     def __init__(self, project_name: str, optim_name: str, config: Optional[dict] = None):
@@ -20,7 +33,6 @@ class WandbLogger:
         self.project_name = project_name
         self.optim_name = f"{optim_name}__{timestamp}"
         self.config = config
-        self.task_iters = dict()
 
     def __enter__(self):
         self.wandb_instance = wandb.init(project=self.project_name, 
@@ -40,13 +52,22 @@ class WandbLogger:
         for value in values:
             self.log_scalar(tag, value)
 
-    def log_gradient_histogram(self, tag: str, parameters: ParameterModule) -> None:
-        self.wandb_instance.log({tag: wandb.Histogram(parameters.continuous_tensors().grad.cpu().numpy())})
+    def log_figure(self, tag: str, figure: plt.Figure) -> None:
+        self.wandb_instance.log({tag: wandb.Image(figure)})
 
-    def get_task_logger(self, task: str) -> 'WandbTaskLogger':
-        task_iter = self.task_iters.get(task, 0)
-        self.task_iters[task] = task_iter + 1
+    def log_gradient_histogram(self, tag: str, parameters: ParameterModule) -> None:
+        flat_grads = flatten_gradients(parameters).cpu().numpy()
+        self.wandb_instance.log({f"{tag}/grad_hist": wandb.Histogram(flat_grads)})
+
+    def log_gradients(self, tag: str, parameters: ParameterModule) -> None:
+        flat_grads = flatten_gradients(parameters)
+        self.wandb_instance.log({f"{tag}/norm": flat_grads.norm().item()})
+        self.wandb_instance.log({f"{tag}/min": flat_grads.abs().min().item()})
+        self.wandb_instance.log({f"{tag}/max": flat_grads.abs().max().item()})
+
+    def get_task_logger(self, task: str, task_iter: int) -> 'WandbTaskLogger':
         return WandbTaskLogger(self.project_name, self.optim_name, task, task_iter)
+
 
 
 class WandbTaskLogger:
@@ -63,17 +84,18 @@ class WandbTaskLogger:
         return self
         
     def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is torch.cuda.OutOfMemoryError:
+        if exc_type is not None and issubclass(exc_type, torch.cuda.OutOfMemoryError):
             current_tags = self.wandb_instance.tags
-            current_tags = () if current_tags is None else current_tags
-            self.wandb_instance.tags = current_tags + ("oom_error",)
+            current_tags = [] if current_tags is None else current_tags
+            self.wandb_instance.tags = [*current_tags, "oom"]
 
         if self.wandb_instance is not None:
             self.wandb_instance.finish()
 
-    def log_scalar(self, tag: str, value: float) -> None:
-        self.wandb_instance.log({tag: value})
+    def log_scalar(self, tag: str, value: float, step: Optional[int] = None) -> None:
+        self.wandb_instance.log({tag: value}, step=step)
 
-    def log_scalars(self, tag, values: list[float] | np.ndarray) -> None:
-        for value in values:
-            self.log_scalar(tag, value)
+    def log_scalars(self, tag, values: list[float] | np.ndarray, step_offset: Optional[int] = None) -> None:
+        for i, value in enumerate(values):
+            step = step_offset + i if step_offset is not None else None
+            self.log_scalar(tag, value, step)
